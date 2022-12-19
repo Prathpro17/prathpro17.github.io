ef5970e52337b125804d321637f3de7a41697617
Prathpro17 <89528625+Prathpro17@users.noreply.github.com>
2022-12-19 18:56:01 +0530

    Add funcs responsible for changing btn color

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-var div = document.getElementById("main-font-div");
+let div = document.getElementById("main-font-div");
 const divText = div.getAttribute('data-text');
 
 for (let i = 0; i < divText.length; i++) {
@@ -8,13 +8,21 @@ for (let i = 0; i < divText.length; i++) {
             ${divText[i].replace(' ', '\u2002')}
         </span>
     `;
-    //console.log(`${i} ` + i*(300- i*t) +  ` ${(i*(300-i*t)) - ((i-1)*(300-(i-1)*t))}`);
-    // i * (300 - i * t)
-    console.log((t*1+(i*100-i*2))-(t*1+((i-1)*100-(i-1)*2)))
 }
 
 
-function buttonHover() {
-    var div2 = document.getElementById("bottom-icons-div");
-    console.log(div2.innerHTML);
+
+function changeButtonColor(index) {
+    for (let i = 1; i < 6; i++) {
+        if (i != parseInt(index)) {
+            document.getElementById(`bottom-btn-${i}`).style.opacity = 0.6;
+        }
+    }
+}
+
+
+function resetButtonColor() {
+    for (let i = 1; i < 6; i++) {
+        document.getElementById(`bottom-btn-${i}`).style.opacity = 1;
+    }
 }
