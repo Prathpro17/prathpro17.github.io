017d2e44a198e9476d8ea6ed2dfe1d092c566195
Prathpro17 <89528625+Prathpro17@users.noreply.github.com>
2022-12-21 19:43:44 +0530

    Fixed `TypeError` issue (i hope)

--- home.js
+++ home.js
@@ -1,34 +1,48 @@
 let div = document.getElementById("main-font-div");
-const divText = div.getAttribute('data-text');
+let divText = div.getAttribute('data-text');
 
 for (let i = 0; i < divText.length; i++) {
-    t = divText.length
-    div.innerHTML += `
-        <span class = "unselectable" style = "animation-delay: ${i * (200 - i * t/2)}ms">
-            ${divText[i].replace(' ', '\u2002')}
-        </span>
-    `;
+    var span = document.createElement('span');
+    span.style.animationDelay = `${i * (200 - (i * (divText.length / 2)))}ms`
+    span.innerHTML = divText[i].replace(' ', '\u2002')
+    div.appendChild(span);
 }
 
 
 for (let i = 1; i < 6; i++) {
-    document.getElementById(`bottom-btn-${i}-div`).style.animationDelay = `${i * 100 + 2700}ms`;
+    document.getElementById(`bottom-btn-${i}-div`).style.animationDelay = `${(i * 100) + 2700}ms`;
 }
 
 
 
 function changeButtonColor(index) {
-    for (let i = 1; i < 6; i++) {
-        if (i != parseInt(index)) {
-            document.getElementById(`bottom-btn-${i}`).style.opacity = 0.6;
+    try {
+        for (let i = 1; i < 6; i++) {
+            if (i != parseInt(index)) {
+                document.getElementById(`bottom-btn-${i}`).style.opacity = 0.5;
+            }
+        }
+    }
+    catch(err) {
+        if (err.name == 'TypeError') {}
+        else {
+            console.log(`ERROR (catched): ${err}`)
         }
     }
 }
 
 
 function resetButtonColor() {
-    for (let i = 1; i < 6; i++) {
-        document.getElementById(`bottom-btn-${i}`).style.opacity = 1;
+    try {
+        for (let i = 1; i < 6; i++) {
+            document.getElementById(`bottom-btn-${i}`).style.opacity = 1;
+        }
+    }
+    catch(err) {
+        if (err.name == 'TypeError') {}
+        else {
+            console.log(`ERROR (catched): ${err}`)
+        }
     }
 }
 
