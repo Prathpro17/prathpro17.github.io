d61b7b8018d64fc2f543677b5a30b9ce412503ba
Prathpro17 <89528625+Prathpro17@users.noreply.github.com>
2022-12-22 18:43:14 +0530

    Added custom cursor

--- home.js
+++ home.js
@@ -23,6 +23,79 @@ for (let i = 1; i < 6; i++) {
 
 
 
+const init_cursor = (options) => {
+
+    let pointer = document.querySelector('.pointer-dot');
+    let ring = document.querySelector('.pointer-ring');
+
+    let mouseX = -100
+    let mouseY = -100
+    let ringX = -100
+    let ringY = -100
+    let isHover = false
+    let mouseDown = false
+
+    let cursor_color = `rgb(100, 217, 236)`
+
+    window.onmousemove = (mouse) => {
+        mouseX = mouse.clientX
+        mouseY = mouse.clientY
+    }
+
+    window.onmousedown = (mouse) => {
+        mouseDown = true
+    }
+
+    window.onmouseup = (mouse) => {
+        mouseDown = false
+    }
+
+    const trace = (a, b, n) => {
+        return (1 - n) * a + n * b;
+    }
+    window['trace'] = trace
+
+    const getOption = (option) => {
+        _ringSize = 10;
+        let defaultObj = {
+            pointerColor: cursor_color,
+            ringSize: _ringSize,
+            ringClickSize: (options['ringSize'] || _ringSize) - 3,
+        }
+        if (options[option] == undefined) {
+            return defaultObj[option]
+        } else {
+            return options[option]
+        }
+    }
+
+    const render = () => {
+        ringX = trace(ringX, mouseX, 0.2)
+        ringY = trace(ringY, mouseY, 0.2)
+
+        if (document.querySelector('button:hover')) {
+            pointer.style.border = `7px solid ${cursor_color}`;
+            pointer.style.top = '0px';
+            pointer.style.left = '0px';
+            isHover = true
+        } else {
+            pointer.style.border = `2px solid ${cursor_color}`;
+            ring.style.padding = '10px';
+            pointer.style.top = '5px';
+            pointer.style.left = '5px';
+            isHover = false
+        }
+
+        pointer.style.transform = `translate(${mouseX}px, ${mouseY}px)`
+        ring.style.transform = `translate(${ringX - getOption('ringSize')}px, ${ringY - getOption('ringSize')}px)`;
+
+        requestAnimationFrame(render)
+    }
+    requestAnimationFrame(render)
+}
+
+
+
 function changeButtonColor(index) {
     try {
         for (let i = 1; i < 6; i++) {
@@ -58,3 +131,4 @@ function resetButtonColor() {
 function redirect(website) {
     window.location.href = `http://www.${website}.com`
 }
+
