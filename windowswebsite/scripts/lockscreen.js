f28ce5cb6f072cd7f346e638f92f33121aea3974
Prathpro17 <89528625+Prathpro17@users.noreply.github.com>
2022-12-28 14:51:36 +0530

    Add files via upload

---
+++ windowswebsite/scripts/lockscreen.js
@@ -0,0 +1,5 @@
+
+function animateLockscreen() {
+    var lockscreen = document.getElementById('lockscreen-container');
+    lockscreen.style.animation = 'lockscreenClickAnimation 500ms ease forwards';
+}
